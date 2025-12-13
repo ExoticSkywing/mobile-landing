6f7245bdf666dcb8b85d8576447d0f563ed7538f
ExoticSkywing <akeemkeebler495@gmail.com>
2025-12-14 02:04:02 +0800

    全场景亮点完善

--- src/config/features.ts
+++ src/config/features.ts
@@ -1,20 +1,22 @@
 import { FiBox, FiStar, FiZap } from "react-icons/fi";
+import { GiCrownedHeart } from "react-icons/gi";
+import { SiNetflix } from "react-icons/si";
 import type { Feature } from "@/types/app";
 
 export const features: Feature[] = [
 	{
-		title: "Main Feature",
-		description: "Describe your main feature here. What makes it special?",
-		icon: FiStar,
+		title: "多巴胺 × 肾上腺素",
+		description: "全程高速兴奋流畅丝滑，拒绝卡顿阻塞，把最好的状态留给自己热爱的每一件事♥",
+		icon: GiCrownedHeart,
 	},
 	{
-		title: "Another Feature",
-		description: "What else can your app do? Tell users about it here.",
+		title: "24H 自动贩卖机",
+		description: "24 小时自助，即买即用，操作简单，0秒使用，无需等待",
 		icon: FiZap,
 	},
 	{
-		title: "One More Feature",
-		description: "Add another key feature of your application here.",
-		icon: FiBox,
+		title: "解锁流媒体服务",
+		description: "无限制访问 Spotify、Netflix、YouTube 等流媒体服务，让您能自由的访问喜欢的内容",
+		icon: SiNetflix,
 	},
 ];
