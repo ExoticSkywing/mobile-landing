3ac105f7eef11249702893abe4621824d916b2e5
ExoticSkywing <akeemkeebler495@gmail.com>
2025-12-14 13:44:03 +0800

    更新展示视频

--- src/config/screenshots.ts
+++ src/config/screenshots.ts
@@ -2,7 +2,7 @@ import type { Screenshots } from "@/types/app";
 
 export const screenshots: Screenshots = {
 	iphone: [
-		"https://api.minio.1yo.cc/nebuluxe/halosparkpix/IMG_3210.MOV",
+		"https://api.minio.1yo.cc/nebuluxe/halosparkpix/IMG_9853.MP4",
 		"https://api.minio.1yo.cc/nebuluxe/halosparkpix/IMG_3240.MOV",
 		"https://api.minio.1yo.cc/nebuluxe/halosparkpix/22777B13-8914-4AE6-AB17-3B498F491DAC.jpeg",
 	],
