7b084a24cb784725f4e116d1678e118cc4a178ab
ExoticSkywing <akeemkeebler495@gmail.com>
2025-12-14 18:28:17 +0800

    完善90%亮点精品展示

--- src/config/screenshots.ts
+++ src/config/screenshots.ts
@@ -2,9 +2,11 @@ import type { Screenshots } from "@/types/app";
 
 export const screenshots: Screenshots = {
 	iphone: [
+		"https://api.minio.1yo.cc/nebuluxe/halosparkpix/IMG_9899.MP4",
 		"https://api.minio.1yo.cc/nebuluxe/halosparkpix/IMG_9858.MP4",
-		"https://api.minio.1yo.cc/nebuluxe/halosparkpix/IMG_3240.MOV",
-		"https://api.minio.1yo.cc/nebuluxe/halosparkpix/22777B13-8914-4AE6-AB17-3B498F491DAC.jpeg",
+		"https://api.minio.1yo.cc/nebuluxe/halosparkpix/IMG_9878.MP4",
+		"https://api.minio.1yo.cc/nebuluxe/halosparkpix/IMG_9903.MOV",
+		"https://api.minio.1yo.cc/nebuluxe/halosparkpix/image-fcgk.png",
 	],
 	ipad: [
 		"/assets/screenshots/ipad/1.png",
