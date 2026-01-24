e3707b6f76f022c3b8eb3b3d861561e985675e0d
ExoticSkywing <akeemkeebler495@gmail.com>
2026-01-25 00:37:52 +0800

    config: update store links and configure pro benefits custom link

--- src/config/site.ts
+++ src/config/site.ts
@@ -10,13 +10,14 @@ export const siteConfig = {
 	// Whether to crop the logo into a fixed square card
 	logoCrop: false,
 	storeLinks: {
-		apple: "https://xingxy.manyuzo.com",
-		google: "https://t.me/paraspacesupport",
+		apple: "https://xingxy.manyuzo.com/shop_cat/netsurf",
+		google: "https://wiki.manyuzo.com/cutting-edge-tech/02.%E5%9B%BD%E9%99%85%E7%BD%91%E7%BB%9C%E5%86%B2%E6%B5%AA/05.telegram/00.intro#%E7%AE%80%E4%BB%8B-%E6%A6%82%E8%BF%B0",
 	},
 	// White Label Config
 	proBenefits: {
 		enabled: true,
 		label: "权益",
+		link: "https://1keyid.1yo.cc", // Custom Link
 	},
 	socialLinks,
 } as const;
