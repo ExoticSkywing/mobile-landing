811312d914b2bd935767a2b49de476fa0b467f7a
ExoticSkywing <akeemkeebler495@gmail.com>
2026-01-27 00:16:49 +0800

    fix: 使用 server 模式 (Astro 5 不支持 hybrid)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "hybrid",
+  output: "server",
   adapter: cloudflare({
     platformProxy: {
       enabled: false,
